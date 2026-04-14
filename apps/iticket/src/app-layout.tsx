05a3fc82fe152f66db9e87ed26880dca4bdb1f5e
Ali Mortazavi <alimori@users.noreply.github.com>
2026-04-14 11:34:53 +0330

    Add files via upload

---
+++ apps/iticket/src/app-layout.tsx
@@ -0,0 +1,19 @@
+import { Header } from "@repo/features/header"
+
+export function AppLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <div className="h-screen flex flex-col bg-background text-foreground">
+
+            {/* Header */}
+            <Header title={'iTicket'} />
+
+            {/* Body */}
+            <div className="flex flex-1 overflow-hidden">
+                {/* Scrollable main */}
+                <main className="flex-1 overflow-y-auto p-6">
+                    {children}
+                </main>
+            </div>
+        </div>
+    )
+}
